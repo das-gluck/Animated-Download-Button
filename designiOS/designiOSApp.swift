12c5265301df295386f20fc1f747fd0c3c8498a5
das-gluck <05gaurav03@gmail.com>
2023-08-23 13:14:23 +0530

    Initial Commit

---
+++ designiOS/designiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  designiOSApp.swift
+//  designiOS
+//
+//  Created by Gaurav Verma on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct designiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
